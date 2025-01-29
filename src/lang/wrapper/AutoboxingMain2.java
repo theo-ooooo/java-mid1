79cb917a5fe9fe45efe06abda2bdb882dd855311
Theo <kkw.theo@gmail.com>
2025-01-29 17:23:25 +0900

    add: 래퍼 클래스 - 오토박싱

---
+++ src/lang/wrapper/AutoboxingMain2.java
@@ -0,0 +1,13 @@
+package lang.wrapper;
+
+public class AutoboxingMain2 {
+    public static void main(String[] args) {
+        int value = 7;
+        Integer boxedValue = value;
+
+        int unboxedValue = boxedValue;
+
+        System.out.println("boxedValue = " + boxedValue);
+        System.out.println("unboxedValue = " + unboxedValue);
+    }
+}
