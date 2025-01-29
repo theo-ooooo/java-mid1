7d63767d8ca8228b48a884a049608949512b2640
Theo <kkw.theo@gmail.com>
2025-01-29 12:43:03 +0900

    add: String 클래스 - 기본

---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+       String str1 = "string";
+       String str2 = new String("string");
+
+        System.out.println("str 1 " + str1);
+        System.out.println("str 2 " + str2);
+    }
+}
