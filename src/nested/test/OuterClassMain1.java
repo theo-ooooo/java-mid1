9f8b784062f840bb077014956dbe1ba001ce8a94
theo-ooooo <kkw.theo@gmail.com>
2025-02-19 22:38:00 +0900

    add: 문제와 풀이1

---
+++ src/nested/test/OuterClassMain1.java
@@ -0,0 +1,9 @@
+package nested.test;
+
+public class OuterClassMain1 {
+    public static void main(String[] args) {
+        OuterClass1.NestedClass nested = new OuterClass1.NestedClass();
+
+        nested.hello();
+    }
+}
